198d0fa757cbc99c0e4c54599a23ca4807bf581a
Snow <Benjamin.Snow@dfo-mpo.gc.ca>
2022-03-23 12:46:01 -0700

    Update HD_Recorder_Rename.py
    Updated Comments Section, clarified code in various areas

--- HD_Recorder_Rename.py
+++ HD_Recorder_Rename.py
@@ -1,29 +1,23 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Dec 12 13:55:48 2021
-
-@author: SnowBe
-"""
-
 from pathlib import Path
 import re
 import os
 
 ########################################## EDITS THESE VALUES ########################################################################
 
-#Generate a Path object for the root directory, where the GoPro recording are located.
+#Generate a Path object for the root directory, where the HD video recording are located.
 
 root_dir = Path("D:/HD Recorder_MiniZeus")
 
 
-###################################### RUN EXIFTOOL TO EXTRACT GOPRO RECORDINGS EXIF TAGS ############################################
+###################################### GENERATE FILES PATHS  ############################################
 
 #Empty list to hold the directory paths for the clean and overlaid MiniZeus video, under the root directory
 
 recording_dirs = []
 
 
-#Empty list to hold GoPro files names, this will be filled using the .iterdir() method on the Path object for the root directory 
+#Empty list to hold HD files names, this will be filled using the .iterdir() method on the Path object for the root directory 
 #where all the files are stored.
 
 recording_names = []
@@ -42,18 +36,22 @@ for each in recording_dirs:
 
 recordings = [str(files) for files in recording_names]
 
-################################################### RENAME HD RECORDER FILES TO CREATED DATE TIMESTAMP ########################
+###########################  RENAME HD RECORDER FILES, DROPPING THE 'SPLIT' SUFFIX AND MILLISECONDS ########################
 
-#Search for the underscores, which break up the components of the file names. Drop everything except the project name, and dive name.
-#The HD recorder names files using the following scheme: ProjectName_transect_videostype_recordingstarttime_-splitXXX.MP4
-#This loop renames the files to the following: ProjectName_transect_videostype_createddate.MP4
-#Also, good idea to drop the milliseconds component, which is the last 3 digits before the file extension.
+"""
+Search for the underscores, which break up the components of the file names. Drop everything except the project name, and dive name.
+The HD recorder names files using the following scheme: ProjectName_transect_videostype_recordingstarttime_-splitXXX.MP4
+This loop renames the files to the following: ProjectName_transect_videostype_createddatetime.MP4
+the code below also search for anre removes  the milliseconds component of the date time
+which is the last 3 digits before the file extension.
+"""
+
 
 recordings_new = []
 
 for each in recordings:
     splits = re.sub("_split\\d{3}", "", each) #Find and remove the portion of the files that is '_splitXXX'
-    millisecs_remove = re.sub("_\\d{3}.mp4", ".mp4", splits) #Find and remove the milliseconds, e.g. "_078_"
+    millisecs_remove = re.sub("_\\d{3}.mp4", ".mp4", splits) #Find and remove a segment of three digits preceeded and followed by an undersocre - i.e. milliseconds, e.g. "_078_"
     recordings_new.append(millisecs_remove)
     
 #Rename the files.
@@ -62,4 +60,3 @@ for i in range(len(recording_names)):
     source_path = recording_names[i]
     dest_path = recordings_new[i]
     os.rename(source_path, dest_path)
-
